03e46f9cffc6202970649f7366ee680fcea1f319
Guillaume Bourgault <skidoodlidoo@gmail.com>
2026-01-14 08:29:34 +0000

    before going to my home computer

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -2,9 +2,13 @@
 # coding: utf-8
 
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
 from tqdm.auto import tqdm
+from datetime import datetime
 import click
+
+url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
 
 dtype = {
     "VendorID": "Int64",
@@ -30,70 +34,93 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+def is_data_present(engine,target_table,
+        year:int, 
+        month:int,)->bool:
+    start = datetime(year, month, 1)
+    end = datetime(year, month+1,1)
+    query = text(f'''
+        SELECT *
+        FROM {target_table}
+        WHERE tpep_pickup_datetime >= :start
+        AND tpep_pickup_datetime < :end
+        LIMIT 1
+        ''', 
+    )
+    with engine.connect() as conn:
+        rows = conn.execute(query, {"start": start, "end": end}).fetchall()
+        print('rows')
+        print(rows)
+    return True
 
 def ingest_data(
-        url: str,
+        year:int, 
+        month:int,
         engine,
         target_table: str,
         chunksize: int = 100000,
+        overwrite:bool=False
 ) -> pd.DataFrame:
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
-    )
+    if overwrite or not is_data_present(engine, target_table, year, month):
+        url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
+        print(url)
+        # before ingesting, check if there.  
+        df_iter = pd.read_csv(
+            url,
+            dtype=dtype,
+            parse_dates=parse_dates,
+            iterator=True,
+            chunksize=chunksize
+        )
 
-    first_chunk = next(df_iter)
+        first_chunk = next(df_iter)
 
-    first_chunk.head(0).to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="replace"
-    )
+        first_chunk.head(0).to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="replace"
+        )
 
-    print(f"Table {target_table} created")
+        print(f"Table {target_table} created")
 
-    first_chunk.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append"
-    )
-
-    print(f"Inserted first chunk: {len(first_chunk)}")
-
-    for df_chunk in tqdm(df_iter):
-        df_chunk.to_sql(
+        first_chunk.to_sql(
             name=target_table,
             con=engine,
             if_exists="append"
         )
-        print(f"Inserted chunk: {len(df_chunk)}")
 
-    print(f'done ingesting to {target_table}')
+        print(f"Inserted first chunk: {len(first_chunk)}")
 
-@click.command()
-@click.option('--pg-user', default='root', help='PostgreSQL username')
-@click.option('--pg-pass', default='root', help='PostgreSQL password')
-@click.option('--pg-host', default='localhost', help='PostgreSQL host')
-@click.option('--pg-port', default='5432', help='PostgreSQL port')
-@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
-def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+        for df_chunk in tqdm(df_iter):
+            df_chunk.to_sql(
+                name=target_table,
+                con=engine,
+                if_exists="append"
+            )
+            print(f"Inserted chunk: {len(df_chunk)}")
+
+        print(f'done ingesting to {target_table}')
+
+def main(
+        year=2025, 
+        month=11, 
+        pg_user='root', 
+        pg_pass='root', 
+        pg_host='pgdatabase', 
+        pg_port='5432', 
+        pg_db='ny_taxi', 
+        chunksize=100000, 
+        target_table='yello_taxi_data', 
+        overwrite=True
+    ):
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-
-    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
-
     ingest_data(
-        url=url,
+        year, 
+        month,
         engine=engine,
         target_table=target_table,
-        chunksize=chunksize
+        chunksize=chunksize,
+        overwrite=overwrite
     )
 
 if __name__ == '__main__':
